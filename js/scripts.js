63a95653e27cacaad17d1d3a6dbc68a6ef4dd2c4
7glaucio <dev@glaucio.me>
2020-05-05 13:38:53 -0400

    Before the fetch implementation

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
 var snippet = [].slice.call(document.querySelectorAll('.hover'));
 if (snippet.length) {
-  snippet.forEach(function (snippet) {
-    snippet.addEventListener('mouseout', function (event) {
-      if (event.target.parentNode.tagName === 'figure') {
-        event.target.parentNode.classList.remove('hover')
-      } else {
-        event.target.parentNode.classList.remove('hover')
-      }
+    snippet.forEach(function (snippet) {
+        snippet.addEventListener('mouseout', function (event) {
+            if (event.target.parentNode.tagName === 'figure') {
+                event.target.parentNode.classList.remove('hover')
+            } else {
+                event.target.parentNode.classList.remove('hover')
+            }
+        });
     });
-  });
 }
+
+
+fetch('https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72')
+    .then(
+    function (response) {
+        return response.json();
+    })
+    .then(function (jsonData) {
+        console.log('Output: ', jsonData);
+    });
