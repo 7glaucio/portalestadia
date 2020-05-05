1f0c6269c047c35d35ac7ee86f61b6bf54e90ff1
7glaucio <dev@glaucio.me>
2020-05-04 23:25:54 -0400

    Home completa com Cards exemplo

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-// Empty JS for your own code to be here
+var snippet = [].slice.call(document.querySelectorAll('.hover'));
+if (snippet.length) {
+  snippet.forEach(function (snippet) {
+    snippet.addEventListener('mouseout', function (event) {
+      if (event.target.parentNode.tagName === 'figure') {
+        event.target.parentNode.classList.remove('hover')
+      } else {
+        event.target.parentNode.classList.remove('hover')
+      }
+    });
+  });
+}
